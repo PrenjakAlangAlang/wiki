09c42498778f30fe100d507d5c57547afed7ccec
Sukron1632 <zoladimas32@gmail.com>
2025-01-20 10:13:33 +0700

    fix history

--- frontend/src/pages/DetailUser.js
+++ frontend/src/pages/DetailUser.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import moment from "moment";
+
 
 const DetailUser = () => {
   const [user, setUser] = useState(null);
